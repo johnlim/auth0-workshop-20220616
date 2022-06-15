81baa7e96ca2ca0e86b09392a8102cefe5cf32bc
John Lim <johnlim.dingwei@gmail.com>
2022-06-15 16:26:50 +0800

    set cache to local storage for debug purposes.

--- frontend/sample-01/src/index.js
+++ frontend/sample-01/src/index.js
@@ -23,6 +23,7 @@ const providerConfig = {
   ...(config.audience ? { audience: config.audience } : null),
   redirectUri: window.location.origin,
   onRedirectCallback,
+  cacheLocation: "localstorage"
 };
 
 ReactDOM.render(
